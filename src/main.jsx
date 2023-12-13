e45f6624b031c4e035e9bbed5a188d4ef45404ae
Pablo Obrach <hernanpabloobrach88@gmail.com>
2023-12-13 16:50:09 -0300

    variable de entorno firebase api

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './main.css'
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: 'nikosecommerce.firebaseapp.com',
+  projectId: 'nikosecommerce',
+  storageBucket: 'nikosecommerce.appspot.com',
+  messagingSenderId: '424043347167',
+  appId: '1:424043347167:web:66eec78aac3ab2e3f87d6a',
+}
+
+initializeApp(firebaseConfig)
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
